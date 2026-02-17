aeb709cae4873abdadf2a29c44f53cf3ea71a3b7
sleepingWolf44 <44sleepingwolf@gmail.com>
2026-02-17 10:58:48 -0600

    Update team.js

---
+++ teamWebsite/team.js
@@ -0,0 +1,12 @@
+function Home() {
+    window.location.href = "./index.html";
+}
+function Menu() {
+    window.location.href = "./menu.html";
+}
+function Contacts() {
+    window.location.href = "./contacts.html";
+}
+function Order() {
+    window.location.href = "./order.html";
+}
